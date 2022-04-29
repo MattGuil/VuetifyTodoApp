9512d9cea81f36f8179e76de4c3c45af1a641217
MattGuil <guillemin.matt@gmail.com>
2022-04-29 17:40:18 +0200

    initial version

---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about pa-6 text-justify">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, illo! Voluptas a laboriosam sint quas ipsum repellendus possimus natus enim, libero deserunt fuga suscipit voluptate exercitationem iure optio repudiandae numquam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, illo! Voluptas a laboriosam sint quas ipsum repellendus possimus natus enim, libero deserunt fuga suscipit voluptate exercitationem iure optio repudiandae numquam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, illo! Voluptas a laboriosam sint quas ipsum repellendus possimus natus enim, libero deserunt fuga suscipit voluptate exercitationem iure optio repudiandae numquam?</p>
+  </div>
+</template>
